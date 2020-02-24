b5ba6c7fdf70a125eec9e237e42e952acfa908fe
githubxinwei <1011692813@qq.com>
2020-02-24 20:30:33 +0800

    git.exe for Windows 国内下载站

---
+++ _posts/2020-02-24-downGitExe.md.php
@@ -0,0 +1,28 @@
+/**
+ * Created by PhpStorm.
+ * User: Xin Wei
+ * Date: 2020/2/24
+ * Time: 20:26
+ */
+---
+layout: post
+title: git.exe下载
+date: 2020-02-24
+categories: git
+tags: [git]
+description: git.exe for Windows 国内下载站。
+---
+
+最近公司用的版本控制是svn，但是最近搞了一个github博客，
+需要使用git，最近去网上下载一个git.exe，下载链接都是国外的，
+真TMD的慢，下载一天都下载不下来，不过我们可以使用迅雷下载，应该会快点，
+今天从网上找到了一个国内下载，很快。
+下载地址：https://github.com/waylau/git-for-win。
+说句实话，个人感觉还是git好用，尤其是更新代码的时候，
+使用svn的时候，我们领导经常问我有哪些文件需要更新，
+真的好烦，但是你要是使用git那就不一样了，
+需要更新哪些功能直接合并分支就ok了。
+客户端的git我们可以使用sourceTree或者TortoiseGit，
+这个看你们公司同事都使用什么了，客户端的看着比较方便，
+而且对于我们需要改一个紧急的bug，我们可以打一个补丁，
+目前没有改好的代码我们可以暂时暂存起来。
