8a3e4b57cc80a813c7bb7779a7f3e5a1497b740d
githubxinwei <1011692813@qq.com>
2021-01-12 09:39:04 +0800

    上传php葵花宝典

---
+++ _posts/2021-01-12-interview.php
@@ -0,0 +1,13 @@
+---
+layout: post
+title: PHP葵花宝典
+date: 2021-01-12
+categories: php
+tags: [php]
+description: PHP面试宝典
+    ---
+
+下面两部葵花宝典是之前的一个同学学习老师给发的：
+
+<a target="_blank" href="./../../../../../img/上卷基础篇（第一版）.pdf">PHP葵花宝典基础篇（上）</a><br />
+<a target="_blank" href="./../../../../../img/下卷高级篇（第一版）.pdf">PHP葵花宝典高级篇（下）</a>
