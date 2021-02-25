642a9949fef39b15e9168183d74a882e6dbf6012
githubxinwei <1011692813@qq.com>
2021-02-25 17:21:49 +0800

    php面试题

---
+++ _posts/2021-02-25-php_interview_questions.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xin Wei
+ * Date: 2021/2/25
+ * Time: 17:15
+ */
+?>
+---
+layout: post
+title: PHP面试题
+date: 2021-02-25
+categories: php
+tags: [php]
+description: PHP面试题
+---
+
+<div id="">1.什么是restful api?</div>
+
+2.接口的安全策略有哪些？
+
+3.mysql的索引类型，主键索引与唯一索引的区别？
+
+4.mysql的隔离级别有哪几种？
+
+5.秒杀怎么设计？
+
+6.什么是脏读？
+
+7.tp框架和laravel框架的区别？
+
+8.redis为什么快？
+
+9.redis的跳表原理？
+
+10.常见排序与时间复杂度?
+
+11.红黑树应用场景？
+
+12.redis 哨兵机制？
+
+13.redis 有序集合是如何实现的？
+
+14.redis 的事务存在的意义？
+
+15.redis 删除策略及原理？
