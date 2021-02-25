b08c0b56bf444bea4dd949c4756ff646a73de3f6
githubxinwei <1011692813@qq.com>
2021-02-25 17:27:53 +0800

    修改php_interview_questions

--- _posts/2021-02-25-php_interview_questions.php
+++ _posts/2021-02-25-php_interview_questions.php
@@ -1,11 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Xin Wei
- * Date: 2021/2/25
- * Time: 17:15
- */
-?>
 ---
 layout: post
 title: PHP面试题
@@ -15,32 +7,32 @@ tags: [php]
 description: PHP面试题
 ---
 
-<div id="">1.什么是restful api?</div>
+<div id="1">1.什么是restful api?</div>
 
-2.接口的安全策略有哪些？
+<div id="2">2.接口的安全策略有哪些？</div>
 
-3.mysql的索引类型，主键索引与唯一索引的区别？
+<div id="3">3.mysql的索引类型，主键索引与唯一索引的区别？</div>
 
-4.mysql的隔离级别有哪几种？
+<div id="4">4.mysql的隔离级别有哪几种？</div>
 
-5.秒杀怎么设计？
+<div id="5">5.秒杀怎么设计？</div>
 
-6.什么是脏读？
+<div id="6">6.什么是脏读？</div>
 
-7.tp框架和laravel框架的区别？
+<div id="7">7.tp框架和laravel框架的区别？</div>
 
-8.redis为什么快？
+<div id="8">8.redis为什么快？</div>
 
-9.redis的跳表原理？
+<div id="9">9.redis的跳表原理？</div>
 
-10.常见排序与时间复杂度?
+<div id="10">10.常见排序与时间复杂度?</div>
 
-11.红黑树应用场景？
+<div id="11">11.红黑树应用场景？</div>
 
-12.redis 哨兵机制？
+<div id="12">12.redis 哨兵机制</div>？
 
-13.redis 有序集合是如何实现的？
+<div id="13">13.redis 有序集合是如何实现的？</div>
 
-14.redis 的事务存在的意义？
+<div id="14">14.redis 的事务存在的意义？</div>
 
-15.redis 删除策略及原理？
+<div id="15">15.redis 删除策略及原理？</div>
