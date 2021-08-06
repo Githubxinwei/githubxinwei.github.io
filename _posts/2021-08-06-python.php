eef8839bf077f208026a32b4731715103d6ca663
Githubxinwei <1011692813@qq.com>
2021-08-06 16:39:43 +0800

    Github报错OpenSSL SSL_connect: Connection was reset in connection to github.com:443终极解决方案

---
+++ _posts/2021-08-06-python.php
@@ -0,0 +1,15 @@
+---
+layout: post
+title: OpenSSL SSL_connect: Connection was reset in connection to github.com:443
+date: 2021-08-06
+categories: github
+tags: [git,github]
+description: OpenSSL SSL_connect: Connection was reset in connection to github.com:443
+---
+
+<?php
+// 爬虫核心功能：获取网页源码
+$html = file_get_contents("https://blog.csdn.net/qq_37555071/article/details/114260533");
+// 通过 php 的 file_get_contents 函数获取百度首页源码，并传给 $html 变量
+echo $html;
+// 输出 $html
