2cbaf0f6d63885fd2b9c1403a466fbc43e040db3
Githubxinwei <1011692813@qq.com>
2021-08-20 19:57:40 +0800

    markdown to html

--- _posts/2021-08-20-nginx.php
+++ _posts/2021-08-20-nginx.php
@@ -7,35 +7,39 @@ tags: [nginx,phpstudy]
 description: phpstudy重启之后出现Nginx报错CreateFile failed
 ---
 
-phpstudy nginx环境,nginx报错日志如下:
+    <p>phpstudy nginx环境,nginx报错日志如下:</p>
 
-2020/04/13 15:40:33 [error] 7844#14252: *7 CreateFile() "D:/phpstudy_pro/WWW/lvblog/public/error/404.html" failed (3: The system cannot find the path specified), client: 127.0.0.1, server: lvblog, request: "GET /note HTTP/1.1", host: "lvblog", referrer: "http://lvblog/"
-在vhost.conf添加配置：（如果请求路径不存在则重写）
+    <p>2020/04/13 15:40:33 [error] 7844#14252: *7 CreateFile() &quot;D:/phpstudy_pro/WWW/lvblog/public/error/404.html&quot; failed (3: The system cannot find the path specified), client: 127.0.0.1, server: lvblog, request: &quot;GET /note HTTP/1.1&quot;, host: &quot;lvblog&quot;, referrer: &quot;http://lvblog/&quot;
+        在vhost.conf添加配置：（如果请求路径不存在则重写）
+    </p>
 
-if (!-e $request_filename) {
-rewrite ^/(.*)$ /index.php/$1 last;
-}
-修改后的如下：
+    <p>if (!-e $request_filename) {
+        rewrite ^/(.*)$ /index.php/$1 last;
+        }
+        修改后的如下：
+    </p>
 
-server {
-listen        80;
-server_name  lvblog;
-root   "D:/phpstudy_pro/WWW/lvblog/public";
-location / {
-index index.php index.html error/index.html;
-autoindex  off;
-if (!-e $request_filename) {
-rewrite ^/(.*)$ /index.php/$1 last;
-}
-}
-location ~ \.php(.*)$ {
-fastcgi_pass   127.0.0.1:9000;
-fastcgi_index  index.php;
-fastcgi_split_path_info  ^((?U).+\.php)(/?.+)$;
-fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
-fastcgi_param  PATH_INFO  $fastcgi_path_info;
-fastcgi_param  PATH_TRANSLATED  $document_root$fastcgi_path_info;
-include        fastcgi_params;
-}
-}
-重启wnmp
+    <p>
+        server {
+        listen        80;
+        server<em>name  lvblog;
+            root   &quot;D:/phpstudy</em>pro/WWW/lvblog/public&quot;;
+        location / {
+        index index.php index.html error/index.html;
+        autoindex  off;
+        if (!-e $request_filename) {
+        rewrite ^/(.<em>)$ /index.php/$1 last;
+            }
+            }
+            location ~ .php(.</em>)$ {
+        fastcgi<em>pass   127.0.0.1:9000;
+            fastcgi</em>index  index.php;
+        fastcgi<em>split</em>path<em>info  ^((?U).+.php)(/?.+)$;
+            fastcgi</em>param  SCRIPT<em>FILENAME  $document</em>root$fastcgi<em>script</em>name;
+        fastcgi<em>param  PATH</em>INFO  $fastcgi<em>path</em>info;
+        fastcgi<em>param  PATH</em>TRANSLATED  $document<em>root$fastcgi</em>path<em>info;
+            include        fastcgi</em>params;
+        }
+        }
+        重启wnmp
+    </p>
