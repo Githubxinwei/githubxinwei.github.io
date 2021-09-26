6d3c8ffe96f9db03faf210de78e738cf41cfb63d
Githubxinwei <1011692813@qq.com>
2021-09-26 18:06:48 +0800

    去掉空格

--- _posts/2021-08-20-nginx.php
+++ _posts/2021-08-20-nginx.php
@@ -13,31 +13,32 @@ description: phpstudy重启之后出现Nginx报错CreateFile failed
 在vhost.conf添加配置：（如果请求路径不存在则重写）
 
 if (!-e $request_filename) {
-rewrite ^/(.*)$ /index.php/$1 last;
+       rewrite ^/(.*)$ /index.php/$1 last;
 }
+
 修改后的如下：
 
 server {
-listen        80;
-server_name  lvblog;
-root   "D:/phpstudy_pro/WWW/lvblog/public";
-location / {
-index index.php index.html error/index.html;
-autoindex  off;
-if (!-e $request_filename) {
-rewrite ^/(.*)$ /index.php/$1 last;
+        listen        80;
+        server_name  lvblog;
+        root   "D:/phpstudy_pro/WWW/lvblog/public";
+        location / {
+            index index.php index.html error/index.html;
+            autoindex  off;
+            if (!-e $request_filename) {
+                  rewrite ^/(.*)$ /index.php/$1 last;
+            }
+        }
+        location ~ \.php(.*)$ {
+            fastcgi_pass   127.0.0.1:9000;
+            fastcgi_index  index.php;
+            fastcgi_split_path_info  ^((?U).+\.php)(/?.+)$;
+            fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
+            fastcgi_param  PATH_INFO  $fastcgi_path_info;
+            fastcgi_param  PATH_TRANSLATED  $document_root$fastcgi_path_info;
+            include        fastcgi_params;
+        }
 }
-}
-location ~ \.php(.*)$ {
-fastcgi_pass   127.0.0.1:9000;
-fastcgi_index  index.php;
-fastcgi_split_path_info  ^((?U).+\.php)(/?.+)$;
-fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
-fastcgi_param  PATH_INFO  $fastcgi_path_info;
-fastcgi_param  PATH_TRANSLATED  $document_root$fastcgi_path_info;
-include        fastcgi_params;
-}
-}
-    重启wnmp</code></pre>
-
+重启wnmp
 <a href="https://blog.csdn.net/sinat_41818709/article/details/105490289">本地摘自此处</a>
+</code></pre>
