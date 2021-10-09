36c3da038b09743448fa9f2d6f4662e07f482240
Githubxinwei <1011692813@qq.com>
2021-10-09 13:38:03 +0800

    单引号变成双引号

--- _posts/2021-10-09-preg.php
+++ _posts/2021-10-09-preg.php
@@ -214,95 +214,95 @@ description: 25个常用的正则表达式
         21、匹配img和src
         const imgReg = /<img.*?src=[\"|\']?(.*?)[\"|\']?\s.*?>/ig
 
-        const htmlStr = '<div></div><img src="sunshine.png" /><img src="sunshine111.png" />'
+        const htmlStr = "<div></div><img src="sunshine.png" /><img src="sunshine111.png" />"
 
         console.log(imgReg.exec(htmlStr))
         // [
-        //   '<img src="sunshine.png" />',
-        //   'sunshine.png',
+        //   "<img src="sunshine.png" />",
+        //   "sunshine.png",
         //   index: 11,
-        //   input: '<div></div><img src="sunshine.png" /><img src="sunshine111.png" />',
+        //   input: "<div></div><img src="sunshine.png" /><img src="sunshine111.png" />",
         //   groups: undefined
         // ]
         console.log(imgReg.exec(htmlStr))
         // [
-        //   '<img src="sunshine111.png" />',
-        //   'sunshine111.png',
+        //   "<img src="sunshine111.png" />",
+        //   "sunshine111.png",
         //   index: 37,
-        //   input: '<div></div><img src="sunshine.png" /><img src="sunshine111.png" />',
+        //   input: "<div></div><img src="sunshine.png" /><img src="sunshine111.png" />",
         //   groups: undefined
         // ]
         复制代码
         22、匹配html中的注释
         const noteReg = /<!--(.*?)-->/g
 
-        const htmlStr = '<!--一个div标签--> <div></div> <!--一个div标签--> <div></div>'
+        const htmlStr = "<!--一个div标签--> <div></div> <!--一个div标签--> <div></div>"
 
         console.log(noteReg.exec(htmlStr))
         // [
-        //   '<!--一个div标签-->',
-        //   '一个div标签',
+        //   "<!--一个div标签-->",
+        //   "一个div标签",
         //   index: 0,
-        //   input: '<!--一个div标签--> <div></div> <!--一个div标签--> <div></div>',
+        //   input: "<!--一个div标签--> <div></div> <!--一个div标签--> <div></div>",
         //   groups: undefined
         // ]
         console.log(noteReg.exec(htmlStr))
         // [
-        //   '<!--一个div标签-->',
-        //   '一个div标签',
+        //   "<!--一个div标签-->",
+        //   "一个div标签",
         //   index: 27,
-        //   input: '<!--一个div标签--> <div></div> <!--一个div标签--> <div></div>',
+        //   input: "<!--一个div标签--> <div></div> <!--一个div标签--> <div></div>",
         //   groups: undefined
         // ]
         复制代码
         23、匹配html中的style
         const styleReg = /style="[^=>]*"([(\s+\w+=)|>])/g
 
-        const htmlStr = '<div style="background:#000;"><span style="color:#fff"></span></div>'
+        const htmlStr = "<div style="background:#000;"><span style="color:#fff"></span></div>"
 
         console.log(styleReg.exec(htmlStr))
         // [
-        //   'style="background:#000;">',
-        //   '>',
+        //   "style="background:#000;">,
+        //   ">",
         //   index: 5,
-        //   input: '<div style="background:#000;"><span style="color:#fff"></span></div>',
+        //   input: "<div style="background:#000;"><span style="color:#fff"></span></div>",
         //   groups: undefined
         // ]
         console.log(styleReg.exec(htmlStr))
         // [
-        //   'style="color:#fff">',
-        //   '>',
+        //   "style="color:#fff">",
+        //   ">",
         //   index: 36,
-        //   input: '<div style="background:#000;"><span style="color:#fff"></span></div>',
+        //   input: "<div style="background:#000;"><span style="color:#fff"></span></div>",
         //   groups: undefined
         // ]
         复制代码
         24、匹配html中的颜色
         const colorReg = /#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})/g
 
-        const htmlStr = '<div style="background:#000;"><span style="color:#fff"></span></div>'
+        const htmlStr = "<div style="background:#000;"><span style="color:#fff"></span></div>"
 
         console.log(colorReg.exec(htmlStr))
         // [
-        //   '#000',
-        //   '000',
+        //   "#000",
+        //   "000",
         //   index: 23,
-        //   input: '<div style="background:#000;"><span style="color:#fff"></span></div>',
+        //   input: "<div style="background:#000;"><span style="color:#fff"></span></div>",
         //   groups: undefined
         // ]
         console.log(colorReg.exec(htmlStr))
         // [
-        //   '#fff',
-        //   'fff',
+        //   "#fff",
+        //   "fff",
         //   index: 49,
-        //   input: '<div style="background:#000;"><span style="color:#fff"></span></div>',
+        //   input: "<div style="background:#000;"><span style="color:#fff"></span></div>",
         //   groups: undefined
         // ]
         复制代码
         25、匹配htmlTag（html标签）
         const endReg = /<("[^"]*"|'[^']*'|[^'">])*>/g
 
-        const htmlStr = '<div style="background:#000;"><span style="color:#fff"></span></div><h1></h1>'
+        const htmlStr = "<div style="background:#000;"><span style="color:#fff"></span></div><h1></h1>"
 
         console.log(endReg.exec(htmlStr))
         console.log(endReg.exec(htmlStr))
