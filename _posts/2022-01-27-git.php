abcffe44692c75c1a01839aa75ee741107b80371
githubxinwei <1011692813@qq.com>
2022-01-27 18:01:44 +0800

    git代理问题

---
+++ _posts/2022-01-27-git.php
@@ -0,0 +1,17 @@
+---
+layout: post
+title: 解决 Failed to connect to github.com port 443:connection timed out
+date: 2022-01-27
+categories: git
+tags: [git]
+description: 解决 Failed to connect to github.com port 443:connection timed out
+---
+
+<pre>
+    <code>
+        取消全局代理：
+        git config --global --unset http.proxy
+
+        git config --global --unset https.proxy
+    </code>
+</pre>
